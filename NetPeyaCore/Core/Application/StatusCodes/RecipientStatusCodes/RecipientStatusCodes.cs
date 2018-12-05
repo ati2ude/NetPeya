94a49685ef760901af5295ce4da07cfb69227e49
Emmanuel <ati2ude1@gmail.com>
2018-12-05 22:28:25 +0200

    #6-crud-for-bankaccounts-entity comment:Change routing to controllers

--- NetPeyaCore/Core/Application/StatusCodes/RecipientStatusCodes/RecipientStatusCodes.cs
+++ NetPeyaCore/Core/Application/StatusCodes/RecipientStatusCodes/RecipientStatusCodes.cs
@@ -7,5 +7,6 @@ namespace Core.Application.StatusCodes.Recipients
     public class RecipientStatusCodes : SharedStatusCodes
     {
         public const int MissingEmailandPhone = 1001;
+        public const int UserNotFound = 1002;
     }
 }
